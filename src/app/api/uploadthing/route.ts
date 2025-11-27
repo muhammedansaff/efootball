13e5b6d6a6b109d89bddec6e3709285f51a1e66b
muhammedansaff <muhammedansaf44@gmail.com>
2025-11-28 00:27:19 +0530

    fixedcomments

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -4,4 +4,7 @@ import { ourFileRouter } from "./core";
 // Export routes for Next App Router
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
+  config: {
+    token: process.env.UPLOADTHING_TOKEN,
+  },
 });
